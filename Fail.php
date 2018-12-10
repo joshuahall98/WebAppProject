4f3cc0e06ed5e2574fc1e4636bac17de6679eacd
joshuahall98 <joshuahall98@live.com>
2018-12-10 19:50:32 +0000

    Add files via upload

---
+++ Fail.php
@@ -0,0 +1,23 @@
+<html>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<center><h2>Failed Login</h2></center>
+
+<form action="index.html">
+<center><input type="submit" value="Try again"></center>
+</form>
+
+</html>
--- connection.php
+++ connection.php
@@ -1,14 +1,52 @@
-<?php
-$servername = "localhost";
-$username = "";
+<?
+
+$inputuser = $_POST['user'];
+$inputpass = $_POST['pass'];
+$user = "root";
 $password = "";
+$database = "registration";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+$connect = mysqli_connect("localhost",$user,$password);
+@mysqli_select_db($database) or ("Database not found");
+
+$query = "SELECT * FROM 'users' WHERE 'user' = '$inputuser'";
+$querypass = "SELECT * FROM 'users' WHERE 'password' = '$inputpass'";
+
+$result = mysqli_query($connect, $query)or die("Error: " . mysqli_error($connect));
+$resultpass = mysqli_query($connect, $querypass)or die("Error: " . mysqli_error($con));
+
+$row = mysqli_fetch_array($result);
+$rowpass = mysqli_fetch_array($resultpass);
+
+$serveruser = $row["user"];
+$serverpass = $row["password"];
+
+if($serveruser&&$serverpass){
+	if(!$result){
+		die("Username or password is invalid");
+		
+	}
+	
+
+
+	echo "<br><center>Database output</b></center><br><br>";
+	mysqli_close();
+	echo $inputpass;
+	echo $serverpass;
+	
+	if($inputpass == $serverpass){
+	
+		header("Location: home.html");
+	
+	}
+	else{
+	
+		header("Location: Fail.php");
+	
+	}
+	}
+
+
+
 ?>
