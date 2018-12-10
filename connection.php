339994513b93b44b7bdb49e97c121b267732313c
joshuahall98 <joshuahall98@live.com>
2018-12-10 21:24:39 +0000

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 $inputuser = $_POST['user'];
 $inputpass = $_POST['pass'];
@@ -7,14 +7,14 @@ $password = "";
 $database = "registration";
 
 
-$connect = mysqli_connect("localhost",$user,$password);
-@mysqli_select_db($database) or ("Database not found");
+$connect = mysqli_connect("localhost",$user,$password,$database)or die("Unable to connect");;
 
-$query = "SELECT * FROM 'users' WHERE 'user' = '$inputuser'";
-$querypass = "SELECT * FROM 'users' WHERE 'password' = '$inputpass'";
+   
+$query = "SELECT 'admin' FROM 'user_login' WHERE 'user' = '$inputuser'";
+$querypass = "SELECT '1234' FROM 'user_login' WHERE 'password' = '$inputpass'";
 
 $result = mysqli_query($connect, $query)or die("Error: " . mysqli_error($connect));
-$resultpass = mysqli_query($connect, $querypass)or die("Error: " . mysqli_error($con));
+$resultpass = mysqli_query($connect, $querypass)or die("Error: " . mysqli_error($connect));
 
 $row = mysqli_fetch_array($result);
 $rowpass = mysqli_fetch_array($resultpass);
